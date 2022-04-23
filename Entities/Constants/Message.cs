c2435da46772e3a9fc3b93afeb26b0de6acb173b
Frederico Jacobi <fredvjacobi@gmail.com>
2022-04-22 21:47:47 -0300

    refactor: messages const file

---
+++ Entities/Constants/Message.cs
@@ -0,0 +1,11 @@
+﻿namespace Entities.Constants;
+
+public static class Message
+{
+    public static string NO_OVERDUE_CLIENTS = "Não há clientes com atraso na devolução.";
+    
+    public static string CLIENT_NOT_FOUND = "Cliente não encontrado.";
+    
+    public static string MOVIE_NOT_FOUND = "Filme não encontrado.";
+    
+}
